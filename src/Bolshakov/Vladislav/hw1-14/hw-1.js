3569ff41be756ed6d5fbd4d07d458998ada3b13b
Vladislav Bolshakov <vladislav.bolshakov@privatbank.ua>
2022-01-15 15:49:28 +0200

    init commit

--- src/Bolshakov/Vladislav/hw1-14/hw-1.js
+++ src/Bolshakov/Vladislav/hw1-14/hw-1.js
@@ -1,7 +1,2 @@
- var age = 6;
-var result = (age < 4) ? true : false;
-if (result == true) {
-   console.log('верно')
-    } else {
-   console.log('не верно') ;
-   }
+
+ let result = a + b < 4 ? true : false;
