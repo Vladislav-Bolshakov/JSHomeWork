bd4bc52325d324a07fdf2d78aac65929b800c117
Vladislav Bolshakov <vladislav.bolshakov@privatbank.ua>
2022-01-16 18:26:50 +0200

    init commit

--- src/Bolshakov/Vladislav/hw1-14/hw-6.js
+++ src/Bolshakov/Vladislav/hw1-14/hw-6.js
@@ -1,3 +1,7 @@
 var arr = [1,2,3,4];
-var sum = arr.reduce(function(a, b) { return a + b; }, 0);
-   console.log(sum);
+var sum=0;
+for(var i=0;i<arr.length;i++){
+    sum = sum + parseInt(arr[i]);
+
+}
+console.log(sum);
