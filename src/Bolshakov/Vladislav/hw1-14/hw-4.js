90258c2d2a433d00b18aeb7dffd4bc2403251c6d
Vladislav Bolshakov <vladislav.bolshakov@privatbank.ua>
2022-01-16 13:28:51 +0200

    init commit

--- src/Bolshakov/Vladislav/hw1-14/hw-4.js
+++ src/Bolshakov/Vladislav/hw1-14/hw-4.js
@@ -1,15 +1,17 @@
 
-var a =1;
-switch (a) {
+var a = +prompt('a?') ;
+
+switch(a) {
 case 0:
-console.log( '0' );
+console.log( 0 );
 break;
-case  1 :
-console.log( '1' );
+
+case 1:
+console.log (1);
 break;
-case 2 || 3:
-console.log( '2,3' );
+
+case 2:
+case 3:
+console.log ('2,3');
 break;
-default:
-console.log("не чего нет ");
 }
